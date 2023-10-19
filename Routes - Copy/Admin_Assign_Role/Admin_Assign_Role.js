1c997430d3338308fbdf7e95712ddda1c16390d5
Rohit Singh <113981097+rohitsingh7879@users.noreply.github.com>
2023-10-19 13:15:03 +0530

    my

--- Routes - Copy/Admin_Assign_Role/Admin_Assign_Role.js
+++
@@ -1,8 +0,0 @@
-const express=require('express')
-const assignroleroute=express.Router()
-const {assignRole,revokeRole}=require('../../Controller/Admin_Assign_Role/Admin_Assign_Role')
-
-assignroleroute.post('/assignrole',assignRole)
-assignroleroute.delete('/revokerole',revokeRole)
-
-module.exports=assignroleroute    
